2bdbbe019f938e7854712587361e598f30623b38
༒ Júlio César ༒ <165187201+Juliocesar1704@users.noreply.github.com>
2025-03-01 16:03:01 -0300

    Torre da escalada V_2.0

--- jogo.py
+++ jogo.py
@@ -1,117 +1,130 @@
-import pygame, sys, random
+import pygame, sys
 
-# Inicialização do Pygame
+# Definindo cores
+
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)      
+BLUE = (0, 0, 255)       
+YELLOW = (255, 255, 0)
+CYAN = (0, 255, 255)   
+MAGENTA = (255, 0, 255)
+ORANGE = (255, 165, 0) 
+PURPLE = (128, 0, 128) 
+BROWN = (165, 42, 42)  
+PINK = (255, 105, 180) 
+GRAY = (128, 128, 128) 
+LIME = (0, 255, 0)     
+TEAL = (0, 128, 128)   
+OLIVE = (128, 128, 0)  
+SLATEBLUE = (106, 90, 205)
+GOLD = (255, 215, 0)
+LAVENDER = (230, 230, 250)
+PEACH = (255, 218, 185)
+MINT = (189, 252, 201)
+LIGHTPINK = (255, 182, 193)
+SKYBLUE = (135, 206, 235)
+LIGHTYELLOW = (255, 255, 224)
+LIGHTGREEN = (144, 238, 144)
+DARKGRAY = (169, 169, 169)
+LIGHTGRAY = (211, 211, 211)
+SILVER = (192, 192, 192)
+DIMGRAY = (105, 105, 105)
+
+
+# Inicializa o relógio e o Pygame
+clock = pygame.time.Clock()
 pygame.init()
-gameDisplay = pygame.display.set_mode((1000, 800))
-pygame.display.set_caption('Torre da escalada')
+
+# Definição do tamanho da tela
+altura = 1600
+largura = 1200
+gameDisplay = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption('Torre da Escalada')
 
 # Carregamento das imagens
 cavaleiroImg = pygame.image.load('cv.jpg')
-cavaleiroW = 50
-cavaleiroH = 50
-cavaleiroImg = pygame.transform.scale(cavaleiroImg, (cavaleiroW, cavaleiroH))
+cavaleiroA = 50
+cavaleiroL = 50
+cavaleiroImg = pygame.transform.scale(cavaleiroImg, (cavaleiroA, cavaleiroL))
 
 vidaImg = pygame.image.load('cc.jpeg')
 vidaImg = pygame.transform.scale(vidaImg, (55, 55))
 
 vidas = 5
 
-# Função para criar plataformas aleatoriamente
-def cria_plataformas():
-    plataformas = []
-    for i in range(10):
-        x = random.randint(0, 950)
-        y = random.randint(100, 750)
-        plataformas.append(pygame.Rect(x, y, 100, 20))
-    return plataformas
 
-# Função para desenhar vidas na tela
-def desenha_vidas(vidas):
-    for i in range(vidas):
-        gameDisplay.blit(vidaImg, (10 + i*60, 10))
+# Mapa
+mapa = [
+    "......VVP...........................................................................................................................................................................",
+    "...............PTTPTT.................................................................................................................................................................",
+    ".....PPTTPP..........................................................................................................................................................................",
+    "....................................................................................................................................................................................",
+    "............PPTTTPP..................................................................................................................................................................",
+    "..............TTT...................................................................................................................................................................",
+    "...PPP................................................................................................................................................................................",
+    ".................PPPPP................................................................................................................................................................",
+    "....................................................................................................................................................................................",
+    ".....PPPP.......................................................................................................................................................................",
+    "....................................................................................................................................................................................",
+    "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+]
 
-# Função para desenhar as plataformas
-def desenha_plataformas(plataformas):
-    for plataforma in plataformas:
-        pygame.draw.rect(gameDisplay, (0, 0, 0), plataforma)
+# Definindo o tamanho dos blocos
+largura_plataforma = largura // 32
+altura_plataforma = altura // 16
 
-# Função de colisão com as plataformas
-def colidir_plataformas(cavaleiro_rect, plataformas):
-    for plataforma in plataformas:
-        if cavaleiro_rect.colliderect(plataforma):
-            return plataforma
-    return None
+largura_base = largura
+altura_base = altura// 10
 
-# Inicialização das variáveis
-CavaleiroX = 500
-cavaleiroY = 700
-velocidade_x = 0
-velocidade_y = 0
-gravidade = 0.5
-salto = -10
-fase = 1
-plataformas = cria_plataformas()
-pulos_disponiveis = 2
-
-FIM = False
 
 # Loop principal do jogo
-while not FIM and vidas > 0 and fase <= 5:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            FIM = True
-        elif evento.type == pygame.KEYDOWN:
-            if evento.key == pygame.K_UP and pulos_disponiveis > 0:
-                velocidade_y = salto
-                pulos_disponiveis -= 1
-            elif evento.key == pygame.K_LEFT:
-                velocidade_x = -3
-            elif evento.key == pygame.K_RIGHT:
-                velocidade_x = 3
-        elif evento.type == pygame.KEYUP:
-            if evento.key == pygame.K_LEFT or evento.key == pygame.K_RIGHT:
-                velocidade_x = 0
+while True:
+    # Verificando eventos
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                pygame.quit()
 
-    # Aplicar gravidade
-    cavaleiroY += velocidade_y
-    velocidade_y += gravidade
-
-    # Atualizar posição do cavaleiro
-    CavaleiroX += velocidade_x
-
-    # Verificar colisões
-    cavaleiro_rect = pygame.Rect(CavaleiroX, cavaleiroY, cavaleiroW, cavaleiroH)
-    plataforma_colidida = colidir_plataformas(cavaleiro_rect, plataformas)
-    if plataforma_colidida and velocidade_y > 0:
-        cavaleiroY = plataforma_colidida.top - cavaleiroH
-        velocidade_y = 0
-        pulos_disponiveis = 2  # Restaurar a quantidade de pulos disponíveis ao colidir com uma plataforma
-
-    # Impedir que o cavaleiro atravesse o chão
-    if cavaleiroY + cavaleiroH > 800:
-        cavaleiroY = 800 - cavaleiroH
-        velocidade_y = 0
-        pulos_disponiveis = 2  # Restaurar a quantidade de pulos disponíveis ao tocar o chão
-
-    # Atualização do display
-    gameDisplay.fill((169,169,169))
-
-    # Desenha as vidas
-    desenha_vidas(vidas)
+    # Desenhando o mapa
+    gameDisplay.fill(BLACK)  # Preenche a tela com a cor preta
     
-    # Desenha as plataformas
-    desenha_plataformas(plataformas)
+    # Desenha os blocos do mapa
+    for linha, i in enumerate(mapa):
+        for coluna in range(0, 16):
+            x = coluna * largura_plataforma
+            y = linha * altura_plataforma
+            bloco = mapa[linha][coluna]
+            cor = BLACK
+            if bloco == 'P':
+                cor_da_plataforma = YELLOW
+                pygame.draw.rect(gameDisplay, cor_da_plataforma, (x, y, largura_plataforma, altura_plataforma))
+            elif bloco == 'B':
+                cor_da_base = GRAY
+                pygame.draw.rect(gameDisplay, cor_da_base, (x, y, largura_base, altura_base))
+            elif bloco == 'T':
+                cor_da_armadilha = RED
+                
+    #COMPRIMENTO DO PERSONAGEM E ARMADILHAS
+    
+    cavaleiro = pygame.dray.rect(mapa,(cavaleiroImg),x,y,40,50)
+    armadilha = pygame.dray.rect(mapa,(RED),x,y,30,35)
+    
+    if armadilha.colliderect(cavaleiro):
+        vidas -= 1
+        cavaleirox = largura//2 - cavaleiroL //2
+        cavaleiroy = largura//2 - cavaleiroA//2
+    if vidas == 0:
+        print('Você MORREU!')
+        pygame.quit
+        print
+        
 
-    # Desenha o cavaleiro
-    gameDisplay.blit(cavaleiroImg, (CavaleiroX, cavaleiroY))
+    pygame.display.update()  # Atualiza a tela
 
-    # Verifica se o cavaleiro alcançou o topo
-    if cavaleiroY < 50:
-        fase += 1
-        CavaleiroX = 500
-        cavaleiroY = 700
-        plataformas = cria_plataformas()
-
-    pygame.display.update()
-
-pygame.quit()
+    # Controla a taxa de quadros por segundo
+    clock.tick(60)
